c81cf5f0eeac39ed998aeaccabba0863c87848a0
Mike0186 <>
2023-04-29 16:54:10 +0000

    Add javascript function structure

---
+++ assets/js/questions.js
@@ -0,0 +1,369 @@
+let generalk = [
+{
+    "question": "In which part of your body would you find the cruciate ligament?"
+    "choices": ["Knee", "Elbow","Neck","Ankle"],
+    "correct": "Knee"
+},
+{
+    "question":"What is the name of the main antagonist in the Shakespeare play Othello"
+    "choices": ["Iago", "Othello","Desdemona","Emilia"],
+    "correct": "Iago"
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},{
+    "question":
+    "choices":
+    "correct":
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+}
+]
+
+let movies = [
+
+    {
+    "question":
+    "choices":
+    "correct":
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct":
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+}
+]
+let history = [
+
+{
+    "question":
+    "choices":
+    "correct":
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},{
+    "question":
+    "choices":
+    "correct":
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+}
+]
+let sports = [
+
+    {
+    "question":
+    "choices":
+    "correct":
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},{
+    "question":
+    "choices":
+    "correct":
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+},
+{
+    "question":
+    "choices":
+    "correct": 
+}
+]
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,36 @@
-console.log("page loaded!");
+
+let questions = document.getElementById("questions");
+let answers = document.getElementsByClassName("btn-answer")
+
+Let score, currentQuestion, questionSet;
+
+function startGame(questions) {
+    score = 0;
+    currentQuestion = 0;
+    questionSet = questions;
+    displayQuestion();
+
+}
+
+function displayQuestion(){ 
+    if(currentQuestion + 1 > questionSet.lenght){
+        gameOver()
+    
+     } else { 
+        questions.innerHTML = questionSet[CurrentQuestion].questions;
+     for(let i =0; i + 1 <= answers.length; i++) {
+        answers[i].innerHTML = questionSet[currentQuestion].choices[i];
+        answers[i].dataset.answer = questionSet[currentQuestion].choices[i];
+     }
+
+     }
+
+}
+
+function checkAnswer(){
+
+}
+
+Function gameOver(){
+
+}
